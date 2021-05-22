2552cc649ebc14acde5208b1f61bb271975eef59
Prabhat Pandey <pandeyprabhat206@gmail.com>
2021-05-23 00:01:05 +0530

    new app called 'main' is added

---
+++ main/views.py
@@ -0,0 +1,8 @@
+from django.http.response import HttpResponse
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+
+
+def index(request):
+    return render(request, 'base.html')
