a3e78ae149d1a1dfb5ee87d6ec5d6cbe5dc7cab0
PrabhatP2000 <pandeyprabhat206@gmail.com>
2021-05-24 13:07:21 +0530

    MySql database added

--- webdev/settings.py
+++ webdev/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'webdev.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'web_dev',
+        'USER': 'prabhat',
+        'PASSWORD': 'admin@123',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 
