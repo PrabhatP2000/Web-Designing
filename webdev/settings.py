876eae778a38046141d5a4a9ca7f6d410e8c199e
PrabhatP2000 <pandeyprabhat206@gmail.com>
2021-05-27 10:51:43 +0530

    SMTP Details Added

--- webdev/settings.py
+++ webdev/settings.py
@@ -123,3 +123,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#EmailBackend
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'avcoe.org.web@gmail.com'
+EMAIL_HOST_PASSWORD = 'avcoe@123'
+EMAIL_BACKEND = (
+        "django.core.mail.backends.smtp.EmailBackend"
+    )
