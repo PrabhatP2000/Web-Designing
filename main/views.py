98a411295cd2f7cbda4da618113ebab2b1d4baee
PrabhatP2000 <pandeyprabhat206@gmail.com>
2021-05-27 10:51:18 +0530

    OTP EMail function added

--- main/views.py
+++ main/views.py
@@ -1,8 +1,24 @@
 from django.http.response import HttpResponse
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse,redirect
+from django.core.mail import send_mail
+from webdev import settings
+import math,random
 
 # Create your views here.
 
-
 def index(request):
     return render(request, 'index.html')
+
+def email(request):
+    digits = [i for i in range(0, 10)]
+    random_str = ""
+    for i in range(6):
+        index = math.floor(random.random() * 10)
+        random_str += str(digits[index])
+    print(random_str,type(random_str))
+    subject = 'AVCOE Remastered OTP Verification'
+    message = 'Thank You for Registering on AVCOE Remastered.\nYour OTP is ' + random_str + '\nUse this to Verify Your Account'
+    email_from = settings.EMAIL_HOST_USER
+    recipient_list = ['Your Email Address']
+    send_mail( subject, message, email_from, recipient_list )
+    return redirect('index')
